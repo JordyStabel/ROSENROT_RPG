e1c6fc0495e89088a4505ed8c037df6875875b8b
Stabel,Jordy J <j.stabel@student.fontys.nl>
2019-06-30 11:26:51 +0200

    added bunch of resources, player collider and scene switching

---
+++ Assets/Scripts/AreaExit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaExit : MonoBehaviour
+{
+    [SerializeField]
+    private string areaToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+            SceneManager.LoadScene(areaToLoad);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
