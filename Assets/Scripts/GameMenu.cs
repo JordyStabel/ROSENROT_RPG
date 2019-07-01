994b23e99d7e013cfa35aac15749e2d8bb3d6d00
Stabel,Jordy J <j.stabel@student.fontys.nl>
2019-07-01 20:46:02 +0200

    added menu navigation, stats window

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -4,17 +4,18 @@ using UnityEngine.UI;
 public class GameMenu : MonoBehaviour
 {
     public GameObject gameMenu;
+    public GameObject[] windows;
     private CharacterStats[] characterStats;
     public Text[] nameText, hpText, mpText, levelText, expText;
     public Slider[] expSlider;
     public Image[] characterImage;
     public GameObject[] characterStatHolder;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    public GameObject[] statusButtons;
 
-    }
+    public Text statName, statHP, statMP, statStength, statDefense, statWeaponEquipped, statWeaponPower, statArmorEquipped, statArmorPower, statExp;
+
+    public Image statImage;
 
     // Update is called once per frame
     void Update()
@@ -23,8 +24,7 @@ public class GameMenu : MonoBehaviour
         {
             if (gameMenu.activeInHierarchy)
             {
-                gameMenu.SetActive(false);
-                GameManager.instance.gameMenuOpen = false;
+                CloseMenu();
             }
             else
             {
@@ -46,10 +46,10 @@ public class GameMenu : MonoBehaviour
                 characterStatHolder[i].SetActive(true);
 
                 nameText[i].text = characterStats[i].characterName;
-                hpText[i].text = $"HP: {characterStats[i].currentHP} / {characterStats[i].maxHP}";
-                hpText[i].text = $"MP: {characterStats[i].currentMP} / {characterStats[i].maxMP}";
+                hpText[i].text = $"HP: {characterStats[i].currentHP}/{characterStats[i].maxHP}";
+                hpText[i].text = $"MP: {characterStats[i].currentMP}/{characterStats[i].maxMP}";
                 levelText[i].text = $"Level: {characterStats[i].level}";
-                expText[i].text = $"{characterStats[i].currentEXP} / {characterStats[i].expToNextLevel[characterStats[i].level]}";
+                expText[i].text = $"{characterStats[i].currentEXP}/{characterStats[i].expToNextLevel[characterStats[i].level]}";
                 expSlider[i].maxValue = characterStats[i].expToNextLevel[characterStats[i].level];
                 expSlider[i].value = characterStats[i].currentEXP;
                 characterImage[i].sprite = characterStats[i].characterImage;
@@ -59,5 +59,62 @@ public class GameMenu : MonoBehaviour
                 characterStatHolder[i].SetActive(false);
             }
         }
+    }
+
+    public void ToggleWindow(int windowIndex)
+    {
+        UpdateMainStats();
+
+        for (int i = 0; i < windows.Length; i++)
+        {
+            if (i == windowIndex)
+            {
+                windows[i].SetActive(!windows[i].activeInHierarchy);
+            }
+            else
+            {
+                windows[i].SetActive(false);
+            }
+        }
+    }
+
+    public void CloseMenu()
+    {
+        for (int i = 0; i < windows.Length; i++)
+        {
+            windows[i].SetActive(false);
+        }
+
+        gameMenu.SetActive(false);
+        GameManager.instance.gameMenuOpen = false;
+    }
+
+    public void OpenStats()
+    {
+        UpdateMainStats();
+
+        // Update all information that will be shown
+        ShowCharacterStats(0);
+
+        for (int i = 0; i < statusButtons.Length; i++)
+        {
+            statusButtons[i].SetActive(characterStats[i].gameObject.activeInHierarchy);
+            statusButtons[i].GetComponentInChildren<Text>().text = characterStats[i].characterName;
+        }
+    }
+
+    public void ShowCharacterStats(int index)
+    {
+        statName.text = characterStats[index].characterName;
+        statHP.text = $"{characterStats[index].currentHP}/{characterStats[index].maxHP}";
+        statMP.text = $"{characterStats[index].currentMP}/{characterStats[index].maxMP}";
+        statStength.text = characterStats[index].strength.ToString();
+        statDefense.text = characterStats[index].defense.ToString();
+        statWeaponEquipped.text = characterStats[index].equippedWeapon != "" ? "None" : characterStats[index].equippedWeapon;
+        statWeaponPower.text = characterStats[index].weaponPower.ToString();
+        statArmorEquipped.text = characterStats[index].equippedArmor != "" ? "None" : characterStats[index].equippedArmor;
+        statArmorPower.text = characterStats[index].armorPower.ToString();
+        statExp.text = (characterStats[index].expToNextLevel[characterStats[index].level] - characterStats[index].currentEXP).ToString();
+        statImage.sprite = characterStats[index].characterImage;
     }
 }
